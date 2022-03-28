8e8ec4b4c41cad171b374c5e739083d541dc1f3f
Yago Barbosa <sciencieboy@gmail.com>
2022-03-28 18:07:52 -0400

    new

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,6 +30,8 @@
             <a href="links.html" class="item">Links</a>
         </div>
    </div>
+
+   <?php echo "Ola'mundo";?>
 
     <br/>
     <br/>
